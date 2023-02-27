e378b32d2b0b5bef75e2ac351b4de014310694c4
Lucas Norberto <lucasnorberto@gmail.com>
2023-02-27 16:51:26 -0300

    home

--- Countdown/script.js
+++ Countdown/script.js
@@ -3,11 +3,71 @@ let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
 
+
+// Formatar data padão Brasileiro
+let data = new Date();
+function formatarData(data){
+  let newDate = new Date(data);
+  return`${newDate.getDate()}/ ${newDate.getMonth()+1}/ ${newDate.getFullYear()} ${newDate.getHours}:${newDate.getMinutes}`;
+}
+
+
+let i = setInterval(countdown,1000);
+countdown();
+
+function capturarData() {
+    const inputDate = document.getElementById('input-date');
+    const dateValue = inputDate.value;
+    const dateObject = new Date(dateValue);
+    if (isNaN(dateObject)) {
+      console.log('Data inválida');
+    } else {
+      console.log('Data: ', dateObject);
+    }
+  }
+
+
+ // Define a data de contagem regressiva
+ const countdownDate = new Date("2022-10-30T23:59:59").getTime();
+          
+
+ // Atualiza o contador a cada segundo
+ const countdownInterval = setInterval(() => {
+   // Obtém a data atual
+   const now = new Date().getTime();
+
+   // Calcula a diferença entre a data atual e a data de contagem regressiva
+   const distance = countdownDate - now;
+
+   // Calcula os dias, horas, minutos e segundos restantes
+   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+   // Exibe o resultado na página
+   const countdownElement = document.getElementById("countdown");
+   countdownElement.innerHTML = `Faltam ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos para o fim do ano.`;
+
+   // Para a contagem regressiva quando a data é alcançada
+   if (distance < 0) {
+     clearInterval(countdownInterval);
+     countdownElement.innerHTML = "Feliz Ano Novo!";
+   }
+ }, 1000);
+
+
+
+
+/* 
+
 //Format: Date(year, month, day, hour, minute)
 //Year is counter from 0 to 11
-let endDate = new Date(2021,8,5,16,30);
+ let endDate = new Date(2023,12,31,23,59);
 //Output value in milliseconds
 let endTime = endDate.getTime();
+
+
 
 function countdown(){
     let todayDate = new Date();
@@ -46,5 +106,4 @@ function countdown(){
         secBox.textContent = addZeroes(secsLeft);
     }
 }
-let i = setInterval(countdown,1000);
-countdown();
+ */
